9cfcb1d572a99c7212cccba3337ece0009d2f240
Abdul Azees <abdulazeespr@gmail.com>
2025-01-21 09:51:23 +0530

    add homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Signup from './pages/Signup'
 import Signin from './pages/Signin'
 import Dashboard from './pages/Dashboard'
 import SendMoney from './pages/SendMoney'
+import HomePage from './pages/HomePage'
 
 function App() {
   
